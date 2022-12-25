59e5ecc8d15584ab7bb0bceb6418632f2164d0a4
RamasamyRM19 <ramasamy.muthiah@ideas2it.com>
2022-12-25 20:24:12 +0530

    New Category Added in Left Container

--- src/app/bottom-component/bottom-left-bar/default-category/default-category.component.ts
+++ src/app/bottom-component/bottom-left-bar/default-category/default-category.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { MENUS } from '../default-category/default-menu';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Menu } from '../menu';
 
 @Component({
@@ -9,25 +8,6 @@ import { Menu } from '../menu';
 })
 export class DefaultCategoryComponent {
 
-  public MENUS: Menu[] = [
-    { id: 1, name: 'My Day', icon: 'fa fa-sun-o', isLastDefaultCategory: false },
-    { id: 2, name: 'Important', icon: 'fa fa-star-o', isLastDefaultCategory: false },
-    { id: 3, name: 'Planned', icon: 'fa fa-calendar-o', isLastDefaultCategory: false },
-    { id: 4, name: 'Assigned to me', icon: 'fa fa-user-o', isLastDefaultCategory: false },
-    { id: 5, name: 'Tasks', icon: 'fa fa-home', isLastDefaultCategory: true }
-  ];
-
-  
-
-  public menus = MENUS;
-  inputTxt = '';
-
-
-
-  addToList() {
-    if (this.inputTxt != '') {
-      this.menus.push({ id: 5, name: 'Tasks', icon: 'fa fa-home', isLastDefaultCategory: true });
-    }
-  }
+  @Input() menus?:Menu[];
 
 }
--- src/app/bottom-component/bottom.component.ts
+++ src/app/bottom-component/bottom.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./bottom.component.scss']
 })
 export class BottomComponent {
-
+  
 }
